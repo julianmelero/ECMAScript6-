013510b100b9da53617b664a6dba35601d39ac9c
Julián Melero <julian@melerohidalgo.es>
2020-08-30 13:59:09 +0200

    Async Await

--- ES8/index.js
+++ ES8/index.js
@@ -27,3 +27,29 @@ const string = "Hello";
 // padStart(cuantos caracteres va a tener la cadena, caracteres )
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, '-----'));
+
+// Async Await
+
+const helloWorld = () => {
+    return new Promise((resolve,reject) => {
+        (true) ? setTimeout( () => resolve('Hello World'), 3000) : reject( new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();    
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);    
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
