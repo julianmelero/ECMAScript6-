cab9046be60c75f3dd26196e4a21d79bbb746f78
Julián Melero <julian@melerohidalgo.es>
2020-08-30 14:15:01 +0200

    E9: regex

--- ES9/index.js
+++ ES9/index.js
@@ -36,3 +36,13 @@ helloWorld()
 .catch(error=> console.log(error))
 .finally( () => console.log('Finalizó'))
 
+// Regex
+
+const regexdata = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+
+const match = regexdata.exec('2018-04-20');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
