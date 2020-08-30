78e37e66e73f468fcc02143e9791ef6d4c636a5f
Julián Melero <julian@melerohidalgo.es>
2020-08-30 13:19:23 +0200

    ES7: Incudes, Exponente

---
+++ ES7/index.js
@@ -0,0 +1,19 @@
+// Includes
+
+// Sirve para conocer si hay un elemento deentro de este valor
+
+let numbers = [1,2,3,7,8];
+
+// var.includes(elemento a buscar, posición donde se empieza)
+
+if(numbers.includes(7,0)){
+    console.log('Sí se encuentra');
+}
+
+// Potencias
+
+let base = 4;
+let exp = 3;
+let result = base**exp;
+console.log(result); //64
+
